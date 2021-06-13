642fcaae033f2bbc70fab7dd3159ed4c487d5cdb
tlpackus <tlpackus@gmail.com>
2021-06-13 15:42:34 -0700

    add files to models and initiate mysql database
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public Recipe()
+    {
+      this.JoinEntities = new HashSet<RecipeTag>();
+    }
+    public int RecipeId { get; set; }
+    public int Name { get; set; }
+
+    [StringLength(10000)]
+    public string Instructions { get; set; }
+
+    [StringLength(1000)]
+    public string Ingredients { get; set; }
+
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<RecipeTag> JoinEntities { get; set; }
+  }
+}
--- RecipeBox/Models/Tag.cs
+++ RecipeBox/Models/Tag.cs
@@ -1,1 +1,16 @@
 using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Tag
+  {
+    public Tag()
+    {
+      this.JoinEntities = new HashSet<RecipeTag>();
+    }
+    public int TagId { get; set; }
+    public string Category { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<RecipeTag> JoinEntities { get; set; }
+  }
+}
