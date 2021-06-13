79527b96fdfe0d5d985b539c890c04e3d460e04c
tlpackus <tlpackus@gmail.com>
2021-06-13 15:06:15 -0700

    add projects structure with boilerplate code and set up for Identity use
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

---
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Tag> Tags { get; set; }
+    public DbSet<Recipe> Recipes { get; set; }
+    public DbSet<RecipeTag> RecipeTag { get; set; }
+
+    public RecipeBoxContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
