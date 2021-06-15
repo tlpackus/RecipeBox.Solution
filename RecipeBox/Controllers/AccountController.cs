d76acaf138c4803185b0fbe3dfd615b760e13fe0
tlpackus <tlpackus@gmail.com>
2021-06-14 20:08:37 -0700

    complete the AccountController and add ViewModels
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>
    
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- RecipeBox/Controllers/AccountController.cs
+++ RecipeBox/Controllers/AccountController.cs
@@ -23,11 +23,48 @@ namespace RecipeBox.Controllers
       return View();
     }
 
-    public ActionResult Register()
+    public IActionResult Register()
     {
       return View();
     }
 
-    // Finish building this out
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
   }
 }
